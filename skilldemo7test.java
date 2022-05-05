990279d24085dc9a950933e99f32e99e3361faa4
hahaocen <103203631+hahaocen@users.noreply.github.com>
2022-05-04 22:25:39 -0700

    Update skilldemo7test.java

--- skilldemo7test.java
+++ skilldemo7test.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class skilldemo7test {
     @Test
     public void addition() {
-        assertEquals(2, skilldemo7.add(2,1));
+        assertEquals(2, skilldemo7.add(1,1)); //1+2=3
     }
 }
